8c1a552ff726f01fd46d0012fea16f23a3e49fb8
elyas mehraein <elyas.mehraein@gmail.com>
2026-08-02 13:22:16 -0700

    one

--- app/src/main/java/com/fitbyai/app/ui/dialogs/ProfileDialog.kt
+++ app/src/main/java/com/fitbyai/app/ui/dialogs/ProfileDialog.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.text.font.FontWeight
@@ -206,4 +207,5 @@ fun ProfileDialog(
             }
         }
     }
+}
 }
